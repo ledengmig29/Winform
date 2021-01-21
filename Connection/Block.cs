fcb5e8f43a8ddcb313abb20820c2b00c27c6c5bc
ledengmig29 <50867425+ledengmig29@users.noreply.github.com>
2021-01-21 11:10:27 +0800

    类

---
+++ Connection/Block.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Connection
+{
+    public class Block
+    {
+        public string BlockTime { get; set; }
+        public string BlockBgSound { get; set; }
+
+        public virtual bool IsPlay()
+        {
+            if (BlockTime == DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Connection/DataConntion.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Connection
+{
+    class DataConntion
+    {
+        public  void Conection()
+        {
+           
+        }
+        public static MySqlConnection Camcon()
+
+        {
+
+            return new MySqlConnection("server = 150.158.155.116; user id = root;password = ZHANGcheng123.;database = memo; Charset=utf8");
+
+        }
+    }
+}
